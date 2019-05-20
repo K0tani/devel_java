5b911bb249881658451d44a5f21e0f7804c0261d
Vladimir Fedotov <iagrenadier@gmail.com>
2019-05-20 13:30:55 +0300

    Переименовал пакеты и класс Point
    Point -> PointC

--- sandbox/src/main/java/ru/stqa/pft/sandbox/point/n2/PointDis.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/point/n2/PointDis.java
@@ -1,15 +1,15 @@
-package ru.stqa.pft.sandbox.point;
+package ru.stqa.pft.sandbox.point.n2;
 
 public class PointDis {
   public static void main(String[] args) {
 
-    Point p1 = new Point(-20, 30);
-    Point p2 = new Point(50, -35);
+    PointC p1 = new PointC(-20, 30);
+    PointC p2 = new PointC(50, -35);
 
     System.out.println("Расстояние между точками " + "p1(" + p1.x + ";" + p1.y + ") и " + "p2(" + p2.x + ";" + p2.y + ") = " + p1.distance(p1, p2));
 
-    Point p3 = new Point(0, 13);
-    Point p4 = new Point(5, -5);
+    PointC p3 = new PointC(0, 13);
+    PointC p4 = new PointC(5, -5);
 
     System.out.println("Расстояние между точками " + "p3(" + p3.x + ";" + p3.y + ") и " + "p4(" + p4.x + ";" + p4.y + ") = " + p1.distance(p3, p4));
   }
