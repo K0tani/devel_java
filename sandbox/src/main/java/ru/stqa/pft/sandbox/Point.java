9a5ee30ecf748bb19763cf543e846b84522016e3
Vladimir Fedotov <iagrenadier@gmail.com>
2019-05-20 13:49:47 +0300

    Revert something

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,16 +1,16 @@
-package ru.stqa.pft.sandbox.point.n2;
+package ru.stqa.pft.sandbox;
 
-public class PointC { //созданый класс
+public class Point { //созданый класс
 
   public double x; //атрибуты
   public double y; //атрибуты
 
-  public PointC(double x, double y) { //конструктор
+  public Point(double x, double y) { //конструктор
     this.x = x; //новые обьекты
     this.y = y; //новые обьекты
   }
 
-  public double distance(PointC p1, PointC p2) { //метод
+  public double distance(Point p1, Point p2) { //метод
     return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
   }
 }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -1,21 +1,20 @@
-package ru.stqa.pft.sandbox.point.Test;
+package ru.stqa.pft.sandbox;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import ru.stqa.pft.sandbox.point.n2.PointC;
 
 public class PointTest {
 
   @Test
   public void testPoint () {
 
-    PointC p1 = new PointC(-20,30);
-    PointC p2 = new PointC(50,-35);
+    Point p1 = new Point(-20,30);
+    Point p2 = new Point(50,-35);
 
     Assert.assertEquals(p1.distance(p1, p2),95.524865872714);
 
-    PointC p3 = new PointC(0,13);
-    PointC p4 = new PointC(5,-5);
+    Point p3 = new Point(0,13);
+    Point p4 = new Point(5,-5);
 
     Assert.assertEquals(p1.distance(p3,p4),18.681541692269406);
   }
