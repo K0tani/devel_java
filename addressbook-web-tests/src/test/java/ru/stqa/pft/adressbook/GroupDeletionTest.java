6865fa30b7b0deb61e7241804301ff59c2184014
Vladimir Fedotov <iagrenadier@gmail.com>
2019-05-26 19:30:09 +0300

    Лекция 2.5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/GroupDeletionTest.java
@@ -1,43 +1,17 @@
 package ru.stqa.pft.adressbook;
 
-import java.util.concurrent.TimeUnit;
-import org.testng.annotations.*;
-import static org.testng.Assert.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-public class GroupDeletionTest {
-  private WebDriver wd;
+import org.testng.annotations.Test;
 
 
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+public class GroupDeletionTest extends TestBase{
 
   @Test
-  public void testCheToTam() throws Exception {
-    wd.get("http://localhost/addressbook/");
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("selected[]")).click();
-    wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Test'])[6]/following::input[2]")).click();
-    wd.findElement(By.linkText("group page")).click();
-    wd.findElement(By.linkText("Logout")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+  public void GroupDeletionTest() throws Exception {
+
+    gotoGroup();
+    selectGroup();
+    deleteSelectedGroup();
+    returnGrPage();
   }
 
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
-    wd.quit();
-  }
 }
