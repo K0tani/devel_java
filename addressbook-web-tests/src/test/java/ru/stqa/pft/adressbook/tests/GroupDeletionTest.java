185d65183c62b6b34dd1131f4dda760f32fd8d5e
Vladimir Fedotov <iagrenadier@gmail.com>
2019-06-10 12:39:27 +0300

    Revert "Revert "Лекция 4.5""
    This reverts commit eed7f772396e3209566a7b6d1382b744822c9446.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupDeletionTest.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupData;
 
+import java.util.List;
+
 
 public class GroupDeletionTest extends TestBase {
 
@@ -11,15 +13,15 @@ public class GroupDeletionTest extends TestBase {
   public void GroupDeletionTest() throws Exception {
 
     app.getNavigation().gotoGroup();
-    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereaGroup()) {
       app.getGroupHelper().createGroup(new GroupData("T2st", null, null));
     }
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroup();
     app.getGroupHelper().returnGrPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before -1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() -1);
 
   }
 
