c48324f9a55cfd52093ba202b14d4f48ed48138d
Vladimir Fedotov <iagrenadier@gmail.com>
2019-06-10 12:31:12 +0300

    Лекция 4.5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTest.java
@@ -4,22 +4,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTest extends TestBase{
 
   @Test
   public void testGroupModification() {
     app.getNavigation().gotoGroup();
-    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereaGroup()) {
       app.getGroupHelper().createGroup(new GroupData("T2st", null, null));
     }
-    app.getGroupHelper().selectGroup();
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillForm(new GroupData("1st", "24", "_332"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnGrPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
 
   }
 }
