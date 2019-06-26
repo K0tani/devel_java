3caadd5eccdc7c144e8539dc3c3a794e41eb8cb2
Vladimir Fedotov <iagrenadier@gmail.com>
2019-06-27 01:08:41 +0300

    Ещё доработки

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
@@ -4,10 +4,18 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.adressbook.appmanager.ApplicationManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
@@ -22,4 +30,17 @@ public class TestBase {
   public ApplicationManager getApp() {
     return app;
   }
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test " + m.getName() + "with parameters " + Arrays.asList(p));
+
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestEnd(Method m,Object[] p) {
+    logger.info("End test " + m.getName() + "with parameters " + Arrays.asList(p));
+
+  }
+
 }
