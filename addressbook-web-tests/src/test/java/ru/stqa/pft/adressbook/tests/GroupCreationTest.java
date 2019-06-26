788c038a00c14da468929195b11bc58aedaa6141
Vladimir Fedotov <iagrenadier@gmail.com>
2019-06-26 15:46:28 +0300

    Лекция 6.11

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTest.java
@@ -3,18 +3,21 @@ package ru.stqa.pft.adressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
-import org.testng.annotations.*;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupData;
 import ru.stqa.pft.adressbook.model.Groups;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Collection;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTest extends TestBase {
 
