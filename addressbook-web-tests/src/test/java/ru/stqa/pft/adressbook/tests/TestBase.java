9523d15e4026043fe3cc47eb25fed2d599183031
Vladimir Fedotov <iagrenadier@gmail.com>
2019-06-27 10:25:50 +0300

    ещё изменения, перенёс файлы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
@@ -1,17 +1,19 @@
 package ru.stqa.pft.adressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import ru.stqa.pft.adressbook.appmanager.ApplicationManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import ru.stqa.pft.adressbook.appmanager.ApplicationManager;
+
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class TestBase {
+
   Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app
@@ -27,10 +29,6 @@ public class TestBase {
     app.stop();
   }
 
-  public ApplicationManager getApp() {
-    return app;
-  }
-
   @BeforeMethod
   public void logTestStart(Method m, Object[] p) {
     logger.info("Start test " + m.getName() + "with parameters " + Arrays.asList(p));
@@ -43,4 +41,7 @@ public class TestBase {
 
   }
 
+  public ApplicationManager getApp() {
+    return app;
+  }
 }
