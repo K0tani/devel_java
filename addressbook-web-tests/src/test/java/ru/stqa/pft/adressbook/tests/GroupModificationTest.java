1660c721ee51d0b63545a0f806a510c3e011af86
Vladimir Fedotov <iagrenadier@gmail.com>
2019-07-04 12:37:54 +0300

    Лекция 7.4

--- addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupModificationTest.java
@@ -11,8 +11,8 @@ public class GroupModificationTest extends TestBase{
 
   @BeforeMethod
   public void ensurePrecondition() {
-    app.goTo().groupPage();
-    if (app.group().all().size() == 0) {
+    if (app.db().groups().size()== 0) {
+      app.goTo().groupPage();
       app.group().create(new GroupData().withName("T2st"));
     }
   }
@@ -20,13 +20,14 @@ public class GroupModificationTest extends TestBase{
   @Test
   public void testGroupModification() {
 
-    Groups before = app.group().all();
+    Groups before = app.db().groups();
     GroupData modifiedGroup = before.iterator().next();
     GroupData group = new GroupData()
             .withId(modifiedGroup.getId()).withName("1st").withHeader("_332").withFooter("24");
+    app.goTo().groupPage();
     app.group().modify (group);
     assertThat(app.group().count(), equalTo(before.size()));
-    Groups after = app.group().all();
+    Groups after = app.db().groups();
     assertThat(after, equalTo(before.without(modifiedGroup).withAdded(group)));
   }
 
